fad16534bf8b1b17b5654656a951f5eddd2d8a7e
Jaime López <exorsnine@gmail.com>
2026-05-22 21:50:03 -0500

    Added invalid argument exception

---
+++ src/main/java/com/example/cixoil/exception/InvalidArgumentException.java
@@ -0,0 +1,7 @@
+package com.example.cixoil.exception;
+
+public class InvalidArgumentException extends RuntimeException {
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+}
