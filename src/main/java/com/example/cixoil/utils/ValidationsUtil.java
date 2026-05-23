5c5eefb90cbe3f0a036659dbde1fc5f22cff537b
Jaime López <exorsnine@gmail.com>
2026-05-22 22:41:48 -0500

    Added static validations

---
+++ src/main/java/com/example/cixoil/utils/ValidationsUtil.java
@@ -0,0 +1,11 @@
+package com.example.cixoil.utils;
+
+import com.example.cixoil.exception.InvalidArgumentException;
+
+public class ValidationsUtil {
+
+    public static void validateId(Long id, String entityName) {
+        if (id == null) throw new InvalidArgumentException("ID de " + entityName + " es null");
+        if (id <= 0) throw new InvalidArgumentException("ID de " + entityName + " inválido");
+    }
+}
