a435c6226e018ca7a564cd88cf43027fa2f6973e
Jaime López <exorsnine@gmail.com>
2026-05-22 23:09:04 -0500

    Excluded UserDetailsConfig from run

--- src/main/java/com/example/cixoil/CixoilApplication.java
+++ src/main/java/com/example/cixoil/CixoilApplication.java
@@ -2,8 +2,9 @@ package com.example.cixoil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class CixoilApplication {
 
 	public static void main(String[] args) {
