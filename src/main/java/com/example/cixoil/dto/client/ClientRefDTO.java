9f322f9c19e7dda1eeaf32067b756cccf203a390
Jaime López <exorsnine@gmail.com>
2026-05-31 02:22:32 -0500

    fix: change dtos for vehicle unit

---
+++ src/main/java/com/example/cixoil/dto/client/ClientRefDTO.java
@@ -0,0 +1,9 @@
+package com.example.cixoil.dto.client;
+
+public record ClientRefDTO(
+        Long id,
+        String name,
+        String fatherLastName,
+        String motherLastName
+) {
+}
--- src/main/java/com/example/cixoil/dto/vehicleunit/VehicleUnitDTO.java
+++ src/main/java/com/example/cixoil/dto/vehicleunit/VehicleUnitDTO.java
@@ -1,14 +1,14 @@
 package com.example.cixoil.dto.vehicleunit;
 
-import com.example.cixoil.dto.client.ClientDTO;
+import com.example.cixoil.dto.client.ClientRefDTO;
 import com.example.cixoil.dto.vehiclemodel.VehicleModelDTO;
-import com.example.cixoil.model.VehicleUseType;
+import com.example.cixoil.dto.vehicleusetype.VehicleUseTypeRefDTO;
 
 public record VehicleUnitDTO(
         Long id,
         VehicleModelDTO vehicleModel,
-        ClientDTO client, // TODO: Cambiar dto sin datos contacto
-        VehicleUseType vehicleUseType,
+        ClientRefDTO client,
+        VehicleUseTypeRefDTO vehicleUseType,
         String plate,
         String color,
         Integer status
