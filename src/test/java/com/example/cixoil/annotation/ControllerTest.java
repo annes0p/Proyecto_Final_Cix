0a05339b0c07b870392e208a410a84c4c80a5427
Jaime López <exorsnine@gmail.com>
2026-06-25 01:54:08 -0500

    test: change DB config for CI

--- src/test/java/com/example/cixoil/annotation/ControllerTest.java
+++ src/test/java/com/example/cixoil/annotation/ControllerTest.java
@@ -1,5 +1,8 @@
 package com.example.cixoil.annotation;
 
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.core.annotation.AliasFor;
@@ -13,6 +16,10 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @WebMvcTest
 @AutoConfigureMockMvc(addFilters = false)
+@ImportAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public @interface ControllerTest {
     @AliasFor(annotation = WebMvcTest.class, attribute = "controllers")
     Class<?>[] controllers() default {};
